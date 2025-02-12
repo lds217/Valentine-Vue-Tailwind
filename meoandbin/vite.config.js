6ad576ce7b53574e4b0c30a174a551d6bf0bda12
lds217 <91755733+lds217@users.noreply.github.com>
2025-02-12 16:09:16 -0500

    smh

---
+++ meoandbin/vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig(({ command, mode }) => ({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      '/github-api': {
+        target: 'https://github-contributions.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/github-api/, '')
+      }
+    }
+  },
+  base: mode === "production" ? "/Valentine-Vue-Tailwind/" : "/",
+}))
